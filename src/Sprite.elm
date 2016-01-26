5883a45a4bdc37406ab36c6dfd1afcbd549eb452
Isaac Shapira <fresheyeball@gmail.com>
2016-01-25 20:03:30 -0700

    ugh gl is hard

--- src/Sprite.elm
+++ src/Sprite.elm
@@ -1,9 +1,5 @@
 module Sprite (..) where
 
-import Html.Attributes exposing (..)
-import Math.Vector2
-import Html exposing (..)
-
 
 type alias Sprite a =
     { a
@@ -13,40 +9,3 @@ type alias Sprite a =
         , size : ( Int, Int )
         , frame : ( Int, Int )
     }
-
-
-render : Sprite a -> List Attribute -> List ( String, String ) -> Html
-render { sheet, rows, columns, size, frame } attrs baseStyle =
-    let
-        px x = toString x ++ "px"
-
-        ( sizeX, sizeY ) = size
-
-        ( frameX, frameY ) = frame
-
-        height = sizeY // rows
-
-        width = Debug.log "width" <| sizeX // columns
-
-        backgroundImage =
-            ( "background-image", "url(" ++ sheet ++ ")" )
-
-        backgroundPosition =
-            let
-                posX = frameX * width * -1 |> px
-
-                posY = frameY * height * -1 |> px
-            in
-                ( "background-position", posX ++ " " ++ posY )
-
-        style' =
-            style
-                <| backgroundImage
-                :: ( "height", px height )
-                :: ( "width", px width )
-                :: ( "display", "block" )
-                :: ( "background-repeat", "no-repeat" )
-                :: backgroundPosition
-                :: baseStyle
-    in
-        node "sprite" (style' :: attrs) []
